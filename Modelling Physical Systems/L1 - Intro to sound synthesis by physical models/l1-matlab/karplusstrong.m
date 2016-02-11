16ceebb6ec98a405379923209089c394ecebf419
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-02-11 16:11:03 +0100

    add all pass filter to control harmonicity

--- Modelling Physical Systems/L1 - Intro to sound synthesis by physical models/l1-matlab/karplusstrong.m
+++ Modelling Physical Systems/L1 - Intro to sound synthesis by physical models/l1-matlab/karplusstrong.m
@@ -1,28 +1,34 @@
 % define input parameters
 freqHz = 500;
-iterations = 200000;
+iterations = 20000;
 fs = 44100;
-lpf_a = 0.5;                                            % low pass filter factor (decaying factor)
+lpf_a = 0.1;                                            % low pass filter factor (decaying factor)
+apf_g = 0.9;                                            % all pass filter factor (harmonicity factor)
 
 % Karplus strong function - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 N = floor(fs/freqHz);                                   % compute delay line length
-x = 2 * rand(1,N) - 1;                                  % generate noise centered at zero
+exciterSignal = 2 * rand(1,N) - 1;                      % generate noise centered at zero
 
 delayline = [zeros(1,N+1)];                             % initialize delayline
 dloffset = length(delayline)-1;                         % compute delay offset
 
-if iterations > length(x)
-    diff = iterations - length(x);
-    x = [x zeros(1,diff)];
+if iterations > length(exciterSignal)                   % exciter signal has to be long 
+    diff = iterations - length(exciterSignal);          % as the number of iterations
+    exciterSignal = [exciterSignal zeros(1,diff)];      %
 end
 
 y = 0;                                                  % initialize new sample
+ylpf = 0;                                               % initialize new sample low pass filtered
 signal = zeros(1,iterations);                           % initialize output signal
 n = 1;                                                  % initialize iterator
 
 while n < iterations
-    y = x(n) + lpf_a * delayline(N) + (1-lpf_a) * delayline(N+1);       % karplus strong with variable low pass filter
+    prev_ylpf = ylpf;
+    ylpf = exciterSignal(n) + lpf_a * delayline(N) ...  % karplus strong with variable low pass filter
+        + (1-lpf_a) * delayline(N+1);       
+    
+    y = - apf_g * ylpf + prev_ylpf + apf_g * delayline(1); % apply all pass filter to control harmonicity
     
     delayline = [y, delayline(1:dloffset)];             % shift delay line and write new sample
     signal(n) = y;                                      % append new sample to output signal
@@ -30,4 +36,4 @@ while n < iterations
     n = n+1;                                            % increase iterator
 end
 figure;plot(signal);title('Simple Karplus Strong algorithm')   % show signal
-% soundsc(signal, fs)                                     % play it
+soundsc(signal, fs)                                     % play it
