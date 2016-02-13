9f34961f1e31115e3977a01cf7009da4016176e9
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-02-13 14:26:47 +0100

    clean up a little

--- Sound and Music Signal Analysis/Lecture 1/smsa-l1-matlab/smsa_l1_exercises2and3.m
+++ Sound and Music Signal Analysis/Lecture 1/smsa-l1-matlab/smsa_l1_exercises2and3.m
@@ -19,11 +19,8 @@ CovMatSignal = eye(L);                                                     % ini
 CovMatNoise = eye(L);                                                      % initialize noise covariance matrix
 filteredy = zeros(size(y));                                                % initialize output signal
 
-% for k = 1:length(y)-L
 while frameIndices(end) <= length(y)
     
-%     signalFrame = y(k:k+L-1);                                              % define frame
-%     noiseFrame = v(k:k+L-1);                                               % define frame
     signalFrame = y(frameIndices);                                              % define frame
     noiseFrame = v(frameIndices);                                               % define frame
     
@@ -31,15 +28,10 @@ while frameIndices(end) <= length(y)
     CovMatSignal = forgetFSignal * CovMatSignal + (1-forgetFSignal) * (signalFrame * signalFrame');
     CovMatNoise = forgetFNoise * CovMatNoise + (1-forgetFNoise) * (noiseFrame * noiseFrame');
        
-%     if mod(k+L-1,L/2) == 0,
 	if mod(frameIndices(end),L/2) == 0,
         Hw = eye(L) - inv(CovMatSignal) * CovMatNoise;                          % compute filter for frame
-%         filteredy(k:k+L-1) = Hw' * window .* signalFrame;                    % filter signal
-        filteredy(frameIndices) = filteredy(frameIndices) + window .* (Hw' * signalFrame);                    % filter signal
-%         observEnh_wiener(ndxEnh) = observEnh_wiener(ndxEnh) + window.*(Hw'*obsBlock);
-    end 
+        filteredy(frameIndices) = filteredy(frameIndices) + window .* (Hw' * signalFrame);    % filter signal
+	end 
     
     frameIndices = frameIndices + 1;
 end
-
-disp('done')
