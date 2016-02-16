7d37130b4db3922432566b51e6ba4bc2be6085f2
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-02-16 10:31:40 +0100

    clean up, add comments. finish

--- Sound and Music Signal Analysis/Lecture 1/smsa-l1-matlab/smsa_l1_exercises2and3.m
+++ Sound and Music Signal Analysis/Lecture 1/smsa-l1-matlab/smsa_l1_exercises2and3.m
@@ -1,44 +1,45 @@
 clearvars -except x v; clc
 %% Load files
 if ~exist('x','var')
-    [x,fs] = audioread('../../data/twoMaleTwoFemale20Seconds.wav');                 % desired signal
-    [v,fs] = audioread('../../data/babble30Seconds.wav');                           % noise signal
+    [x,fs] = audioread('../../data/twoMaleTwoFemale20Seconds.wav');        % desired signal
+    [v,fs] = audioread('../../data/babble30Seconds.wav');                  % noise signal
 end
 %% Exercise 3:
 
 desired_snr = 0;                                                           % define desired SNR
 L = 40;                                                                    % define filter length
-forgetFSignal = 0.2;%985;                                                     % forgetting factor for speech
-forgetFNoise = 0.2;%995;                                                      % forgetting factor for noise
+forgetFSignal = 0.2;%985;                                                  % forgetting factor for speech
+forgetFNoise = 0.2;%995;                                                   % forgetting factor for noise
 
 y = mixatsnr(x,v,desired_snr);                                             % mix signals at desired SNR
 
-frameIndices = 1:L;
-window = hann(L,'periodic');
+frameIndices = 1:L;                                                        % initialize iterator indices
+window = hann(L,'periodic');                                               % create window to overlap
 CovMatSignal = eye(L);                                                     % initialize signal covariance matrix
 CovMatNoise = eye(L);                                                      % initialize noise covariance matrix
 filteredy = zeros(size(y));                                                % initialize output signal
+regulPar = 1e-10;                                                          % initialize regularization factor
 
-regulPar = 1e-10;
 while frameIndices(end) <= length(y)
     
-    signalFrame = y(frameIndices);                                              % define frame
-    noiseFrame = v(frameIndices);                                               % define frame
+    signalFrame = y(frameIndices);                                         % define signal frame
+    noiseFrame = v(frameIndices);                                          % define noise frame
     
     % compute covariance matrices of signal and noise
     CovMatSignal = forgetFSignal * CovMatSignal + (1-forgetFSignal) * (signalFrame * signalFrame');
     CovMatNoise = forgetFNoise * CovMatNoise + (1-forgetFNoise) * (noiseFrame * noiseFrame');
     
-    CovMatSignal = CovMatSignal*(1-regulPar)+...
+    % regularize signal covariance matrix
+    CovMatSignal = CovMatSignal*(1-regulPar)+...                           
         (regulPar)*trace(CovMatSignal)/(L)*...
         eye(L);
-       
+
+    % overlap 50% of frame length
 	if mod(frameIndices(end),L/2) == 0,
-        % Hw = eye(L) - inv(CovMatSignal) * CovMatNoise;                    % compute filter for frame
         Hw = eye(L) - CovMatSignal\CovMatNoise; % compute filter for frame.
                                                 % X = A\B is the solution to the equation AX = B
         filteredy(frameIndices) = filteredy(frameIndices) + window .* (Hw' * signalFrame);    % filter signal
 	end 
     
-    frameIndices = frameIndices + 1;
+    frameIndices = frameIndices + 1;                                       % increase iterator
 end
