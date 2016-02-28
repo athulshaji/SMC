4671ce14c3091c841ae09efdee02a4db7f14715c
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-02-28 17:02:19 +0100

    add mixatsnr function

--- Sound and Music Signal Analysis/_matlabscripts/smsa_library.m
+++ Sound and Music Signal Analysis/_matlabscripts/smsa_library.m
@@ -2,6 +2,7 @@ function message = smsa_library
   assignin('base','stft_AnalysisAndSynthesis',@stft_AnalysisAndSynthesis);
   assignin('base','stft_Analysis',@stft_Analysis);
   assignin('base','stft_Synthesis',@stft_Synthesis);
+  assignin('base','mixatsnr',@mixatsnr);
   message='Done importing functions to workspace';
 end
 
@@ -112,4 +113,20 @@ function y = stft_Synthesis(mX,pX,N,soundlength)
         pin = pin + H;                                                     % advance sound pointer
         idx = idx + 1;                                                     % go to next column in magnitude matrix
     end
+end
+
+function [output] = mixatsnr(clean,noise,desired_snr)
+% Exercise 1: Make a MATLAB function that is able to mix a desired speech 
+% signal with noise at a certain average input SNR level.
+
+desired_snr = db2mag(desired_snr);                  % compute magnitude of desired SNR (db to mag)
+
+desirednoise_var = var(clean) / desired_snr;        % compute variance of attenuated noise 
+
+gain = sqrt(desirednoise_var / var(noise));         % compute attenuation gain
+
+minlen = min(length(clean),length(noise));          % get the length of the shortest signal
+
+output = clean(1:minlen) + gain * noise(1:minlen);  % mix signals
+
 end
