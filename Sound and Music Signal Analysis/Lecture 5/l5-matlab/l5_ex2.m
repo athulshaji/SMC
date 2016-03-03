707e3383f162ea1d71ea805aa84d35b6497187c1
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-03-03 17:44:24 +0100

    make signal complex. add more harmonics to signal

--- Sound and Music Signal Analysis/Lecture 5/l5-matlab/l5_ex2.m
+++ Sound and Music Signal Analysis/Lecture 5/l5-matlab/l5_ex2.m
@@ -3,23 +3,28 @@
 % (assume complex signals, use, e.g., analytic.m) and test the estimator on your signals from exercise 1. 
 % Describe the differences you observe for the various noise levels.
 clear; clc
-addpath('../../_matlabscripts')                                            % add the path of the smsa library
+addpath('../../_smsa-matlab')                                            % add the path of the smsa library
 disp(smsa_library);                                                        % initialize smsa library, used for Vandermonde
 
 %% Generate signal and add noise
-desired_snr = 30;                                                           % desired SNR relation between signal and noise
+desired_snr = 0;                                                           % desired SNR relation between signal and noise
 fs = 44100;                                                                % sampling frequency
 f = 440;                                                                   % fundamental frequency
+L = 10;                                                                     % number of harmonics
 A = 0.8;                                                                   % amplitude of the signal
 duration = 1;                                                              % duration of the signal in seconds
 N = fs * duration;                                                         % size of signal in samples
 t = 0:1/fs:duration-1/fs;                                                  % time array of the signal
 
-signal = A * sin(2*pi*f*t);                                                % create signal
+signal = zeros(size(t));                                                   % initialize signal
+for n = 1:L
+    signal = signal + (A/n) * sin(2*pi*f*n*t);                             % create signal
+end
+signal = signal/max(signal);
 x = awgn(signal,desired_snr,'measured');                                   % add gaussian noise to the signal
-
+x = analytic(x)';                                                          % make the signal complex
 %% 
-L = 5;                                                                     % number of harmonics
+% L = 5;                                                                     % number of harmonics
 w_array = 400:500;
 M = 1000;
 sgmt = x(1:M);
@@ -29,7 +34,7 @@ for i = 1:length(w_array)
     Z = createVandermondeMat(w,L,M);
     estimated_w(i) = power(norm(Z'*sgmt',2),2);
 end
-[val idx] = max(estimated_w)
+[val idx] = max(estimated_w);
 w_array(idx)
 
 %%
