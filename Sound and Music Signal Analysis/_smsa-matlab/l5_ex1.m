74a67f443f15e94b8549a7e18c118af08e171fd9
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-03-04 17:39:49 +0100

    comb filtering works

--- Sound and Music Signal Analysis/_smsa-matlab/l5_ex1.m
+++ Sound and Music Signal Analysis/_smsa-matlab/l5_ex1.m
@@ -3,14 +3,14 @@
 % Generate a synthetic harmonic signal (assume a fixed model order) and add white Gaussian noise 
 % resulting in different SNRs (0 dB, 10 dB, 20 dB), 
 % and test your implementation using the signals you generated (calculate the MSE). 
-
+clear; clc
 %% Generate signal and add noise
-desired_snr = 0;                                                           % desired SNR relation between signal and noise
-fs = 44100;                                                                % sampling frequency
+desired_snr = 30;                                                           % desired SNR relation between signal and noise
+fs = 8000;                                                                % sampling frequency
 f = 440;                                                                   % fundamental frequency
 L = 10;                                                                     % number of harmonics
 A = 0.8;                                                                   % amplitude of the signal
-duration = 1;                                                              % duration of the signal in seconds
+duration = 5;                                                              % duration of the signal in seconds
 N = fs * duration;                                                         % size of signal in samples
 t = 0:1/fs:duration-1/fs;                                                  % time array of the signal
 
@@ -22,15 +22,6 @@ signal = signal/max(signal);
 x = awgn(signal,desired_snr,'measured');                                   % add gaussian noise to the signal
 
 %% Pitch estimation with Comb filtering
-lowestfreq = 100;                                                          % lowest frequency considered
-highestfreq = 1000;                                                        % highest frequency considered
-tau = round(fs/highestfreq):1:round(fs/lowestfreq);                        % all periods considered
-
-MSE = zeros(length(tau),1);                                                % initialize Mean Squared Error array
-for i = 1:length(tau)                                                      % for all the periods
-    x_delayed = x(tau(i)+1:end);                                           % delay signal
-    e = x(1:end-tau(i)) - x_delayed;                                       % compute the error between the n sample
-    MSE(i) = (1/(N-tau(i))) * sum(power(e,2));                             % compute mean squared error for this tau (period)
-end
-[y,i] = min(MSE);                                                          % find the minimum mean squared error
-estimatedfreq = fs/tau(i)                                                  % estimated frequency: period to frequency
+maxperiod = 50;
+[mse, estimatedperiod] = l5_combfilter(x,maxperiod);
+estimatedfreq = fs/estimatedperiod
