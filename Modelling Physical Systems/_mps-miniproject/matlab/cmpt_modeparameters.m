af7b421edae37155e16e2c50675e960c80377222
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-04-24 19:08:24 +0200

    cmpt_modeparameters: delayline is computed outside

--- Modelling Physical Systems/_mps-miniproject/matlab/cmpt_modeparameters.m
+++ Modelling Physical Systems/_mps-miniproject/matlab/cmpt_modeparameters.m
@@ -1,9 +1,9 @@
-function [phi,R,theta,A0,d,delayline] = cmpt_modeparameters(B,fm,fs);
+function [phi,R,theta,A0,d] = cmpt_modeparameters(B,fm,fs)
 phi = 2 * pi * (fm/fs);                                                    % resonance frequency
 % parameters for the poles
 R = 0.99-B/2;                                                              % filter bandwidth
 theta = acos((2 * R * cos(phi)) / (1 + R * R));                            %
 A0 = (1 - R * R) * sin(theta);                                             % gain
 d = floor(fs/fm);                                                          % compute delay line length
-delayline = zeros(1,d);                                                  % initialize delayline
+% delayline = zeros(1,d);                                                  % initialize delayline
 end
