d007c2fea2f635239f8d412d07c969d10ec4ba43
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2016-06-03 17:35:36 +0200

    bandedwg: add cosine impact and more

--- Modelling Physical Systems/_mps-miniproject/matlab/bandedwg.m
+++ Modelling Physical Systems/_mps-miniproject/matlab/bandedwg.m
@@ -1,28 +1,36 @@
 clear;clc
-nmodes      = 3;                                                           % number of modes
-f0          = 440;                                                         % fundamental frequency
-fm          = f0*(1:nmodes);                                               % frequency for each mode
-iterations  = 20000;
+nmodes      = 1;                                                           % number of modes
+% f0          = 234.8;%440;                                                         % fundamental frequency
+% fm          = f0*[1,2.97,1.97];%(1:nmodes);                                               % frequency for each mode
+fm          = [443];%[139,322,527,689,1033,1324,1518];
+% fm          = [527,689,1033,1324,1518];
+iterations  = 100000;
 fs          = 44100;                                                       % sampling frequency
 fb_gain     = 0.995;                                                        % feedback gain. < 1
-
+a = 0.5;
 % create excitation - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 % noise:
-% N = ceil(fs/f0);
+% N = ceil(fs/fm(1));
 % x = 2 * rand(1,N) - 1;
 
-% impact:
-Fm = 0.6;
-forceduration = 0.002;
-t = 0:1/fs:forceduration;
-x = Fm * (1 - cos(2*pi*t/forceduration));
+% cosine impact:
+% Fm = 0.6;
+% forceduration = 0.002;
+% t = 0:1/fs:forceduration;
+% x = Fm * (1 - cos(2*pi*t/forceduration));
+
+% recorded impulse:
+x = audioread('littlehang1note_443.wav')';% x = x(1:47500)';
+
 
 if iterations > length(x)
     x = [x zeros(1,iterations - length(x))];
+else
+    iterations = length(x);
 end
 
 % Band pass filter parameters - - - - - - - - - - - - - - - - - - - - - - -
-B           = 0.01*ones(1,nmodes);
+B           = [10,10,5].*ones(1,nmodes);
 phi         = zeros(1,nmodes);
 R           = zeros(1,nmodes);
 theta       = zeros(1,nmodes);
@@ -32,12 +40,13 @@ delayline   = zeros(nmodes,floor(fs/fm(1)));
 for m = 1:nmodes                                                           % compute parameters for each mode :
     phi(m) = 2 * pi * (fm(m)/fs);                                          % resonance frequency
     % parameters for the poles
-    R(m) = 0.99-B(m)/2;                                                    % filter bandwidth
+%     R(m) = 0.99-B(m)/2;                                                    % filter bandwidth
+    R(m) = exp(-pi*B(m)*(1/fs));
     theta(m) = acos((2 * R(m) * cos(phi(m))) / (1 + R(m) * R(m)));         %
     A0(m) = (1 - R(m) * R(m)) * sin(theta(m));                             % gain
     d(m) = floor(fs/fm(m));                                                % compute delay line length
 end
-
+% m = 1;freqz([1,0,-1],[1,-2*R(m)*cos(theta(m)),R(m)*R(m)]);
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 signal  = zeros(1,iterations);                                             % initialize output signal
 y       = zeros(1,nmodes);                                                 % initialize new sample
@@ -50,7 +59,7 @@ for n = 1:iterations
     
     for m = 1:nmodes                                                       % for each mode
         y(m) = A0(m) * (x(n) - xm2(m) + 2 * R(m) * cos(theta(m)) * ym1(m) - R(m) * R(m) * ym2(m)); % band pass filter differential equation
-        
+               
         out(m) = y(m) + fb_gain * delayline(m,d(m));                       % output sample: sum sample to delayed sample
         delayline(m,1:d(m)) = [out(m),delayline(m,1:d(m)-1)];              % store output sample to circular delay
         
@@ -65,5 +74,7 @@ for n = 1:iterations
     signal(n) = sum(out);
     
 end
-plot(signal)
+figure;plot(signal)
 soundsc(signal,fs)
+
+% better impact
